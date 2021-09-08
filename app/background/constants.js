c7115a9188b0b679985c05dee715d7664a4f869c
Jan Drewniak <jan.drewniak@gmail.com>
2021-09-08 11:02:01 -0400

    Removing unused webRequest permissions and filters

--- app/background/constants.js
+++ app/background/constants.js
@@ -13,29 +13,3 @@ export const defaultUIOptions = {
     visible: { value: false }
 }
 
-export const webRequestFilters = {
-    urls: [
-        "https://*.wikipedia.org/wiki/*",
-        "https://*.wikipedia.org/w/index.php*",
-        "https://commons.wikimedia.org/wiki/*",
-        "https://commons.wikimedia.org/w/index.php*",
-        "https://*.wikibooks.org/wiki/*",
-        "https://*.wikibooks.org/w/index.php*",
-        "https://*.wikiversity.org/wiki/*",
-        "https://*.wikiversity.org/w/index.php*",
-        "https://*.wikivoyage.org/wiki/*",
-        "https://*.wikivoyage.org/w/index.php*",
-        "https://*.wikinews.org/wiki/*",
-        "https://*.wikinews.org/w/index.php*",
-        "https://*.wikiquote.org/wiki/*",
-        "https://*.wikiquote.org/w/index.php",
-        "https://*.wiktionary.org/wiki/*",
-        "https://*.wiktionary.org/w/index.php",
-        "https://www.wikidata.org/wiki/*",
-        "https://www.wikidata.org/w/index.php",
-        "https://www.mediawiki.org/wiki/*",
-        "https://www.mediawiki.org/w/index.php",
-        "https://office.wikimedia.org/wiki/*",
-        "https://office.wikimedia.org/w/index.php"
-    ]
-};
