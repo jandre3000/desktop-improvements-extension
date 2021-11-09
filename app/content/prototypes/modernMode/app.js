5e0b4c90d2d80017d7fde7b5f43d1e4757e9f695
Jan Drewniak <jan.drewniak@gmail.com>
2021-11-09 11:27:03 -0500

    Add VectorStickyHeader to modern mode prototype

--- app/content/prototypes/modernMode/app.js
+++ app/content/prototypes/modernMode/app.js
@@ -1,15 +1,24 @@
 const url = new URL( window.location.href );
 
-if ( !url.searchParams.has( 'useskinversion') ) {
+if (
+    !url.searchParams.has( 'useskinversion') ||
+    !url.searchParams.has( 'vectorstickyheader')
+) {
     url.searchParams.append('useskinversion', '2');
+    url.searchParams.append('vectorstickyheader', '1');
+
     window.location.search = url.searchParams.toString();
 }
 
 document.addEventListener( 'click', ( event ) => {
     if ( event.target.href ) {
         const url = new URL( event.target.href );
-        if ( !url.searchParams.has( 'useskinversion' ) ) {
+        if (
+            !url.searchParams.has( 'useskinversion' ) ||
+            !url.searchParams.has( 'vectorstickyheader')
+            ) {
             url.searchParams.set( 'useskinversion', '2' );
+            url.searchParams.set('vectorstickyheader', '1');
             event.target.href = url.toString();
         }
     }
