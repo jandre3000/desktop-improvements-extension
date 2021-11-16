8213315423350bb52a641693981e5c660fa45e83
Jan Drewniak <jan.drewniak@gmail.com>
2021-11-16 14:57:38 -0500

    Removing stickyHeader from modern mode preview.

--- app/content/prototypes/modernMode/app.js
+++ app/content/prototypes/modernMode/app.js
@@ -1,24 +1,15 @@
 const url = new URL( window.location.href );
 
-if (
-    !url.searchParams.has( 'useskinversion') ||
-    !url.searchParams.has( 'vectorstickyheader')
-) {
+if ( !url.searchParams.has( 'useskinversion') ) {
     url.searchParams.append('useskinversion', '2');
-    url.searchParams.append('vectorstickyheader', '1');
-
     window.location.search = url.searchParams.toString();
-}
+};
 
 document.addEventListener( 'click', ( event ) => {
     if ( event.target.href ) {
         const url = new URL( event.target.href );
-        if (
-            !url.searchParams.has( 'useskinversion' ) ||
-            !url.searchParams.has( 'vectorstickyheader')
-            ) {
+        if ( !url.searchParams.has( 'useskinversion' ) ) {
             url.searchParams.set( 'useskinversion', '2' );
-            url.searchParams.set('vectorstickyheader', '1');
             event.target.href = url.toString();
         }
     }
