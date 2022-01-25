d5ff66294ea35cff0fd331dc94e79e90e63db341
Jan Drewniak <jan.drewniak@gmail.com>
2022-01-25 10:01:27 -0500

    Changing modern mode url param to useskin

--- app/content/prototypes/modernMode/app.js
+++ app/content/prototypes/modernMode/app.js
@@ -1,15 +1,17 @@
 const url = new URL( window.location.href );
+const newSkinParamName = 'useskin';
+const newSkinParamValue = 'vector-2022';
 
-if ( !url.searchParams.has( 'useskinversion') ) {
-    url.searchParams.append('useskinversion', '2');
+if ( !url.searchParams.has( newSkinParamName ) ) {
+    url.searchParams.append( newSkinParamName, newSkinParamValue );
     window.location.search = url.searchParams.toString();
 };
 
 document.addEventListener( 'click', ( event ) => {
     if ( event.target.href ) {
         const url = new URL( event.target.href );
-        if ( !url.searchParams.has( 'useskinversion' ) ) {
-            url.searchParams.set( 'useskinversion', '2' );
+        if ( !url.searchParams.has( newSkinParamName ) ) {
+            url.searchParams.set( newSkinParamName, newSkinParamValue );
             event.target.href = url.toString();
         }
     }
