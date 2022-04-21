a386c735cf03a0524582017bbb9c2335812cfbf5
Jan Drewniak <jan.drewniak@gmail.com>
2022-04-21 00:40:22 -0400

    Version 0.0.14
    - FIX: Disable the modern mode script on action pages.

--- app/content/prototypes/modernMode/app.js
+++ app/content/prototypes/modernMode/app.js
@@ -2,17 +2,25 @@ const url = new URL( window.location.href );
 const newSkinParamName = 'useskin';
 const newSkinParamValue = 'vector-2022';
 
-if ( !url.searchParams.has( newSkinParamName ) ) {
-    url.searchParams.append( newSkinParamName, newSkinParamValue );
-    window.location.search = url.searchParams.toString();
-};
+(() => {
 
-document.addEventListener( 'click', ( event ) => {
-    if ( event.target.href ) {
-        const url = new URL( event.target.href );
-        if ( !url.searchParams.has( newSkinParamName ) ) {
-            url.searchParams.set( newSkinParamName, newSkinParamValue );
-            event.target.href = url.toString();
-        }
+    if ( url.searchParams.has( 'action') ) {
+        return;
     }
-});
+
+    if ( !url.searchParams.has( newSkinParamName ) ) {
+        url.searchParams.append( newSkinParamName, newSkinParamValue );
+        window.location.search = url.searchParams.toString();
+    };
+
+    document.addEventListener( 'click', ( event ) => {
+        if ( event.target.href ) {
+            const url = new URL( event.target.href );
+            if ( !url.searchParams.has( newSkinParamName ) ) {
+                url.searchParams.set( newSkinParamName, newSkinParamValue );
+                event.target.href = url.toString();
+            }
+        }
+    });
+
+})();
